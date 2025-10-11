f897d785cc0bd9b52d70fe5b523b5cbdbdc02612
DashaKovalevskaya <miriadadasha1@gmail.com>
2025-10-11 09:56:29 +0300

    lesson_complete

---
+++ lesson-01/01.js
@@ -0,0 +1,12 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+const max_num = 20
+let sum = 0 //сумма чисел
+let num = 1 //число
+while (num <= max_num) {
+    sum += num
+    num += 1
+}
+console.log(sum);
