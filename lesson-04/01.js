0b6d01c7d9fc3cbe91a145ca96026f8e962f5772
DashaKovalevskaya <miriadadasha1@gmail.com>
2025-10-25 09:24:00 +0300

    lesson_4_complete_1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -18,9 +18,8 @@ function includesElement(array, element) {
     for(i = 0; i < array.length; i++){
         if(element === array[i]) {
         return true
-        } else {
-        return false
-        }
+        }  
     }
+    return false
 }
 console.log(includesElement([1, 2, 3], 1));
